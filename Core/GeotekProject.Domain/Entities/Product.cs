a7008f68426e921043eeca8ed31a3abffd1f339c
Fırat IŞILDAK <firatisildak1@gmail.com>
2024-09-09 19:35:59 +0300

    first commit

---
+++ Core/GeotekProject.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using GeotekProject.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeotekProject.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+    }
+}
