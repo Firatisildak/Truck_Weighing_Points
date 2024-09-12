a4b2ca27a4992838aa9a994a8d1cee9c652e120f
Fırat IŞILDAK <firatisildak1@gmail.com>
2024-09-12 21:53:12 +0300

    fronend first commit

---
+++ BlazorWASM_GeotekProject/BlazorWASM_GeotekProject/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWASM_GeotekProject.Models
+{
+    public class Product
+    {
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public int ModelId { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
